80faeac6f66ce32795ea3ba6051c6b1089a56628
himmat <sundramverma79810@gmail.com>
2026-06-14 01:11:10 +0530

    Fix admin base path

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/admin/',
+
   plugins: [react()],
 
   server: {
@@ -28,8 +29,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-
-    // Vite ka default minifier use karega
     chunkSizeWarningLimit: 1000,
   },
 
