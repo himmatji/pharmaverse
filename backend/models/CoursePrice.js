dcd9eec54bdcfa9614f5e654c4a8645530d11440
himmat <sundramverma79810@gmail.com>
2026-07-10 08:38:56 +0530

    Fix CoursePrice schema

--- backend/models/CoursePrice.js
+++ backend/models/CoursePrice.js
@@ -1,42 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const PriceSchema = new mongoose.Schema(
+  {
+    price: {
+      type: Number,
+      default: 99,
+      min: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100,
+    },
+  },
+  { _id: false }
+);
 
 const CoursePriceSchema = new mongoose.Schema({
   prices: {
     type: Map,
-    of: new mongoose.Schema({
-      price: { 
-        type: Number, 
-        default: 99,
-        min: 0
-      },
-      discount: { 
-        type: Number, 
-        default: 0,
-        min: 0,
-        max: 100
-      }
-    }),
-    default: {
-      "BPharm": { price: 99, discount: 0 },
-      "DPharm": { price: 79, discount: 0 },
-      "MPharm": { price: 149, discount: 0 },
-      "PharmD": { price: 129, discount: 0 },
-      "PhD": { price: 199, discount: 0 }
-    }
+    of: PriceSchema,
+    default: () =>
+      new Map([
+        ["BPharm", { price: 99, discount: 0 }],
+        ["DPharm", { price: 79, discount: 0 }],
+        ["MPharm", { price: 149, discount: 0 }],
+        ["PharmD", { price: 129, discount: 0 }],
+        ["PhD", { price: 199, discount: 0 }],
+      ]),
   },
-  updatedAt: { 
-    type: Date, 
-    default: Date.now 
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
   },
+
   updatedBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Admin'
-  }
+    ref: "Admin",
+  },
 });
 
-// Update timestamp on save
-CoursePriceSchema.pre('save', function () {
+CoursePriceSchema.pre("save", function (next) {
   this.updatedAt = new Date();
+  next();
 });
 
-module.exports = mongoose.model('CoursePrice', CoursePriceSchema);
+module.exports = mongoose.model("CoursePrice", CoursePriceSchema);
